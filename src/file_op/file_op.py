b11df6cd857bd0d134337be81cd3868f34063417
Cheng <zxcvb6958@gmail.com>
2018-05-10 16:41:33 +0800

    delete many images

--- src/file_op/file_op.py
+++ src/file_op/file_op.py
@@ -8,7 +8,13 @@ txt_dir = 'txt_1000'
 
 image_files = os.listdir(os.path.join(root, image_dir))
 txt_files = os.listdir(os.path.join(root, txt_dir))
+count = 1
 
 for image, txt in zip(image_files, txt_files):
-    print('{0} vs {1}'.format(image[:-4], txt[:-4]))
-    pdb.set_trace()
+    if image[:-4] == txt[:-4]:
+        print('{0} vs {1}'.format(image[:-4], txt[:-4]))
+        os.renames(os.path.join(root, image_dir, image), os.path.join(root, image_dir, str(count) + '.jpg'))
+        os.renames(os.path.join(root, txt_dir, txt), os.path.join(root, txt_dir, str(count) + '.txt'))
+        count += 1
+    else:
+        raise UserWarning('image is not corresponding to txt')
