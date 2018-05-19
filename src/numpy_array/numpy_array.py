a015faeec083647360f3d94a309fa3ed942d06f0
zxcvb6958 <zxcvb6958@gmail.com>
2018-05-19 22:17:31 +0800

    update numpy_array.py

--- src/numpy_array/numpy_array.py
+++ src/numpy_array/numpy_array.py
@@ -1,4 +1,13 @@
 import numpy
 
 
+# (1, 100) -> (25, 4)
 a = numpy.arange(100).reshape(25, 4).tolist()
+
+
+# (1, 5) + (1, 5) = (2, 5)
+b = numpy.arange(5).reshape(1, 5)
+c = numpy.arange(5).reshape(1, 5)
+d = numpy.vstack((b, c))
+# print(d.shape)
+# print(d)
