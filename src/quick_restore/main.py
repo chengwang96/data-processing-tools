f5451941044574e3df061e9c1d44bb283233ac1f
Cheng <zxcvb6958@gmail.com>
2019-11-03 01:21:42 +0800

    add file format

--- src/quick_restore/main.py
+++ src/quick_restore/main.py
@@ -2,7 +2,7 @@
 """
 @author: zxcvb6958
 @date: 2019/10/17
-@last modified: 2019/10/17
+@last modified: 2019/11/03
 """
 
 import pickle
@@ -14,11 +14,11 @@ def readPickle(input, from_python_type='3'):
     now_ver = sys.version[0]
 
     if from_python_type == now_ver:
-        with open(input) as f:
+        with open(input, 'rb') as f:
             return pickle.loads(f.read())
 
     if from_python_type == '2' and now_ver == '3':
-        with open(input) as f:
+        with open(input, 'rb') as f:
             return pickle.load(f, encoding='latin1')
 
     if from_python_type == '3' and now_ver == '2':
@@ -29,11 +29,11 @@ def writePickle(data, save_path, to_python_type='3'):
     now_ver = sys.version[0]
 
     if now_ver == 2 or to_python_type == now_ver:
-        with open(save_path) as f:
+        with open(save_path, 'wb') as f:
             f.write(pickle.dumps(data))
 
     if to_python_type == '2' and now_ver == '3':
-        with open(save_path) as f:
+        with open(save_path, 'wb') as f:
             f.write(pickle.dumps(data, protocol=2))
 
 
